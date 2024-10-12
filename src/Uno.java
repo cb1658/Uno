c91214012f59c01bd619cb04e78c228368457e9d
League Student <league@iMac-26.attlocal.net>
2024-10-12 11:26:23 -0700

    First commit

---
+++ src/Uno.java
@@ -0,0 +1,211 @@
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Uno {
+	
+	private static ArrayList<Player> players = new ArrayList<Player>();
+	private static Random rand = new Random();
+	private static Card topOnDeck;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Uno Banana Edition (TM)!");
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Scanner sc = new Scanner(System.in);
+		WL: while(true) {
+			
+			if(players.size() < 2) {
+				System.out.println("Enter your name here -> ");
+				String temp = sc.nextLine();
+				players.add(new Player(temp));
+			}
+			else {
+				System.out.println("Enter your name here, or if all players are entered, type 'END' -> ");
+				String temp = sc.nextLine();
+				if(temp.toUpperCase().equals("END")) {
+					break WL;
+				}
+				else {
+					players.add(new Player(temp));
+				}
+			}
+		}
+		
+		// Players loaded...
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("----------------------");
+		StringBuilder bob = new StringBuilder();
+		bob.append("Welcome ");
+		for(int i = 0; i<players.size()-1; i++) {bob.append(players.get(i).getName() + ", ");}bob.append("and "+players.get(players.size()-1).getName()+"!");
+		System.out.println(bob.toString());
+		
+		System.out.println();
+		
+		// Randomizing top cards and giving random cards to the players...
+		
+		if(rand.nextInt(100) < 30) {
+			topOnDeck = new Card(Card.colors[rand.nextInt(Card.colors.length)], Card.properties[rand.nextInt(Card.properties.length)], Card.nums[rand.nextInt(10)]);
+		}
+		else {
+			topOnDeck = new Card(Card.colors[rand.nextInt(Card.colors.length)], Card.nums[rand.nextInt(10)]);
+		}
+		
+		for(Player p : players) {
+			p.drawCard();
+			p.drawCard();
+			p.drawCard();
+			p.drawCard();
+		}
+		
+		////////// BELOW IS FOR DEBUGGING PURPOSES
+		
+		/*System.out.println("TOP CARD ON DECK:" + topOnDeck.printCard());
+		
+		for(Player p : players) {
+			System.out.println(p.getName() +" -->  " +p.printAllCards());
+		}*/
+		
+		////////// ABOVE IS FOR DEBUGGING PURPOSES
+		
+		GAME: while(true) {
+			
+			for(int i = 0; i < players.size(); i++) {
+				
+				System.out.println("THE TOP CARD IS..." + topOnDeck.printCard());
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				System.out.println("Player " + players.get(i).getName() + " ... here are your cards: " + players.get(i).printAllCards());
+				
+				System.out.println("Which one would you like to draw?");
+				sc.nextLine();
+			}
+		}
+	}
+}
+
+class Card{
+	
+	private final String color;
+	private final String property;
+	private final int number;
+	
+	public final static String[] properties = new String[]{"Wild 4 / NO COLOR", "Draw 2", "Draw 2", "Reverse", "Reverse", "Reverse", "Reverse"};
+	public final static String[] colors = new String[] {"Red", "Yellow", "Green", "Blue"};
+	public final static int[] nums = new int[] {1,2,3,4,5,6,7,8,9};
+	
+	public Card(String color, String property, int number) {
+		super();
+		this.color = color;
+		this.property = property;
+		this.number = number;
+	}
+	public Card(String color, int number) {
+		super();
+		this.color = color;
+		this.number = number;
+		this.property = "none";
+	}
+	
+	public String printCard() {
+		if(this.getProperty() == "none") {
+			return " " + this.getColor() + " " + this.getNumber() + " ";
+		}
+		else if(this.getProperty() == "Wild 4 / NO COLOR") {
+			return " WILD 4 ";
+		}
+		else {
+			return " " + this.getProperty() + " " + this.getColor() + " "; 
+		}
+	}
+	
+	public String getColor() {
+		return this.color;
+	}
+	
+	public String getProperty() {
+		return this.property;
+	}
+	public int getNumber() {
+		return this.number;
+	}
+	
+	
+	public boolean matches(Card c) {
+		if(this.color == c.getColor() || this.number == c.getNumber() || this.property == c.getProperty()) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+}
+
+class Player{
+	
+	private static Random rand = new Random();
+	
+	private ArrayList<Card> cards = new ArrayList<Card>();
+	
+	private String name;
+	
+	public Player(String n) {
+		this.name = n;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void drawCard() {
+		
+		if(rand.nextInt(100) < 30) {
+			cards.add(new Card(Card.colors[rand.nextInt(Card.colors.length)], Card.properties[rand.nextInt(Card.properties.length)], Card.nums[rand.nextInt(9)]));
+		}
+		else {
+			cards.add(new Card(Card.colors[rand.nextInt(Card.colors.length)], Card.nums[rand.nextInt(9)]));
+		}
+	}
+	
+	public String printCard(Card c) {
+		if(c.getProperty() == "none") {
+			return " " + c.getColor() + " " + c.getNumber() + " ";
+		}
+		else if(c.getProperty() == "Wild 4 / NO COLOR") {
+			return " WILD 4 ";
+		}
+		else {
+			return " " + c.getProperty() + " " + c.getColor() + " "; 
+		}
+	}
+	
+	public String printAllCards() {
+		
+		StringBuilder bob = new StringBuilder();
+		for(Card c : cards) {
+			bob.append(this.printCard(c));
+			bob.append("|");
+		}
+		
+		return bob.toString().substring(0, bob.length()-1);
+	}
+	
+	public ArrayList<Card> getCards(){
+		return cards;
+	}
+	
+}
